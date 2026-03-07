e7215e58154d83ac4707d83a297bcc9971ddfd12
Navid Atashbar <atashbar@uci.edu>
2026-03-06 20:27:30 -0800

    added comments and starter code for what else we need to do

---
+++ model.py
@@ -0,0 +1,22 @@
+from process import clean_data
+
+def train_knn():
+    #To do:
+    #call clean data
+    #predict the model
+    pass
+
+def train_logi():
+    #To do:
+    #call clean data
+    #predict the model
+    pass
+
+def train_nn():
+    #To do:
+    #call clean data
+    #predict the model
+    pass
+
+if __name__ == '__main__':
+    pass
