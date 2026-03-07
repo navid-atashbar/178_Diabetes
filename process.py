5cefc436bc35367434d4eec83534c3da23179900
Navid Atashbar <atashbar@uci.edu>
2026-03-06 20:22:58 -0800

    finished the processing part that has the training data

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 #101,766 rows 50 cols
 #drop weight
@@ -17,7 +18,19 @@ def clean_data(files):
     df = df[df["diag_2"] !='?']
     df = df[df["diag_3"] !='?']
     df.replace('?', "Unknown", inplace=True)
-
+    #now turn the readmition into number 0 for NOT readdmited, 1 for Readmiited less than 30 days
+    #2 for READMITTED AFTER 30
+    df['readmitted'] = df['readmitted'].map({'NO':0, '>30':1,'<30':2})
+    # print(df['readmitted'].value_counts())
+    #TURNS EVERYTHING  stringINTO 1 0 T F etc instead of words
+    df = pd.get_dummies(df)
+    # print(df.shape)
+    #TO DOOO MAKE THE SPLIT
+    X = df.drop(["readmitted"], axis=1)
+    y = df["readmitted"]
+    x_rest, x_te, y_rest, y_te = train_test_split(X, y, test_size=0.2, random_state=42)
+    x_tr, x_val, y_tr, y_val = train_test_split(x_rest, y_rest, test_size=0.25, random_state=42)
+    return x_tr, x_val,x_te, y_tr, y_val, y_te
 if __name__ == "__main__":
-    clean_data("./diabetes+130-us+hospitals+for+years+1999-2008/diabetic_data.csv")
-
+    x_tr, x_val,x_te, y_tr, y_val, y_te = clean_data("./diabetes+130-us+hospitals+for+years+1999-2008/diabetic_data.csv")
+    print(x_tr.shape, x_val.shape, x_te.shape)
