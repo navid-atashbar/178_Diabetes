3d5aa228785163e71e48a0215d60160f986f1d6e
navida2 <atashbar@uci.edu>
2026-03-14 00:01:39 -0700

    optimized the 4 models to get higher results

--- model.py
+++ model.py
@@ -19,7 +19,18 @@ def train_knn(x_tr, y_tr, x_val, y_val):
     #To do:
     #call clean data
     #predict the model
-    knn = KNeighborsClassifier(n_neighbors = 5)
+    best_k = 5
+    accuracy_best  = 0
+    for k in [3,5,7,11,15]:
+
+        knn = KNeighborsClassifier(n_neighbors = k)
+        knn.fit(x_tr, y_tr)
+        acc = accuracy_score(y_val, knn.predict(x_val))
+        print(f"K-NN Accuracy K={k}: {acc:.4f}")
+        if acc > accuracy_best:
+            accuracy_best = acc
+            best_k = k
+    knn = KNeighborsClassifier(n_neighbors=best_k)
     knn.fit(x_tr, y_tr)
     evaluate_model("K-Nearest Neighbors", knn, x_val, y_val)
 
@@ -31,7 +42,7 @@ def train_logistic(x_tr, y_tr, x_val, y_val):
     #To do:
     #call clean data
     #predict the model
-    log_regression = LogisticRegression(max_iter=1000)
+    log_regression = LogisticRegression(max_iter=1000,class_weight='balanced',C=0.1)
     log_regression.fit(x_tr, y_tr)
     evaluate_model("Logistic Regression", log_regression, x_val, y_val)
 
@@ -43,7 +54,7 @@ def train_nn(x_tr, y_tr, x_val, y_val):
     #To do:
     #call clean data
     #predict the model
-    nn = MLPClassifier(hidden_layer_sizes=(64,32), max_iter = 500, random_state = 42)
+    nn = MLPClassifier(hidden_layer_sizes=(256,128,64), max_iter = 500, early_stopping=True,validation_fraction=0.1,random_state = 42)
     nn.fit(x_tr, y_tr)
     evaluate_model("Neural Network", nn, x_val, y_val)
 
@@ -52,7 +63,7 @@ def train_nn(x_tr, y_tr, x_val, y_val):
     print(f"Model saved to {filename}")
 
 def train_rf(x_tr, y_tr, x_val, y_val):
-    rf = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+    rf = RandomForestClassifier(n_estimators=200, max_depth=20,class_weight='balanced',random_state=42)
     rf.fit(x_tr, y_tr)
     evaluate_model("Random Forest", rf, x_val, y_val)
 
